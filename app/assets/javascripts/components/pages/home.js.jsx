1672546f8eabccdf01e070d2cf8e519b7127cc50
Owen Rapaport <owenssrapaport@aol.com>
2015-05-12 16:41:23 -0400

    styling and a little more work

--- app/assets/javascripts/components/pages/home.js.jsx
+++ app/assets/javascripts/components/pages/home.js.jsx
@@ -2,7 +2,7 @@ var Home = React.createClass({
   //TODO: What to add to this view?
   render: function() {
     return (
-      <div className="main">
+      <div className="main" id="home">
         <Feed data={this.props.data} />
       </div>
     );
